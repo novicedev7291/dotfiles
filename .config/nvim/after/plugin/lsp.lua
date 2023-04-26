f2d3bb3eaccf572c56037e484ac3787c505cbd2e
kuldeep <kuldeepyadav7291@gmail.com>
2023-04-26 23:50:36 +0530

    Add some rust analyzers function

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -2,31 +2,35 @@ local lsp = require('lsp-zero')
 
 lsp.preset('recommended')
 
---lsp.ensure_installed({
+lsp.ensure_installed({
 --	"eslint",
 --	"sumneko_lua",
 --	"tsserver",
---	"rust_analyzer"
---})
+	"rust_analyzer"
+})
 
 local cmp = require('cmp')
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
 	['C-p'] = cmp.mapping.select_prev_item(cmp_select),
 	['C-n'] = cmp.mapping.select_next_item(cmp_select),
-	['C-y'] = cmp.mapping.confirm({ select = true }),
-	['C-space'] = cmp.mapping.complete(),
+	['<CR>'] = cmp.mapping.confirm({ select = true }),
+	['<C-Space>'] = cmp.mapping.complete(),
 })
+local cmp_completion = {
+    autocomplete = false
+}
 
 lsp.set_preferences({
 	sign_icons = {}
 })
 
 lsp.setup_nvim_cmp({
+    completion = cmp_completion,
 	mapping = cmp_mappings
 })
 
-lsp.on_attach(function(client, bufnr)
+local function on_attach(_, bufnr)
 	local opts = { buffer = bufnr, remap = false }
 
 	vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
@@ -41,6 +45,52 @@ lsp.on_attach(function(client, bufnr)
 	vim.keymap.set('n', ']d', function() vim.diagnostic.goto_prev() end, opts)
 	vim.keymap.set('n', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
 
-end)
+end
+
+lsp.on_attach(on_attach)
+
+require("lspconfig").rust_analyzer.setup({
+    on_attach=on_attach,
+    settings = {
+        ["rust-analyzer"] = {
+            imports = {
+                granularity = {
+                    group = "module",
+                },
+                prefix = "self",
+            },
+            cargo = {
+                buildScripts = {
+                    enable = true,
+                },
+            },
+            procMacro = {
+                enable = true
+            },
+            checkOnSave = {
+                command = "clippy",
+            },
+        }
+    }
+})
 
 lsp.setup()
+
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  update_in_insert = false,
+  underline = true,
+  severity_sort = false,
+  float = true,
+})
+
+
+local format_sync_grp = vim.api.nvim_create_augroup("Format", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.rs",
+  callback = function()
+    vim.lsp.buf.format({ timeout_ms = 100 })
+  end,
+  group = format_sync_grp,
+})
